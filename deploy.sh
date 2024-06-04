c25aaf18e2f52acb50c957d5545c13c701e888e9
wenger <wenger_an@163.com>
2024-06-04 19:10:41 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -182,7 +182,7 @@ handle_deploy_files() {
     rm -rf $gh_pages_directory/$version/$language
   fi
   cp -r $build_directory/* $gh_pages_directory
-  cp -r $build_directory/spot/vremotes/origin/usdt/* $gh_pages_directory/usdt_swap/v1/
+  cp -r $build_directory/spot/vremotes/origin/usdt/cn/* $gh_pages_directory/usdt_swap/v1/cn/
 }
 
 check_diff() {
