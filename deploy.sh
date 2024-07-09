2bbd4e0cda5b2ec6b7481b1ad8199b1ab12d11ce
wenger <wenger_an@163.com>
2024-07-09 14:56:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -181,7 +181,6 @@ handle_deploy_files() {
   if [ -d "$gh_pages_directory/$version/$language" ]; then
     rm -rf $gh_pages_directory/$version/$language
   fi
-  cp -r $build_directory/* $gh_pages_directory
   cp -r $build_directory/spot/vremotes/origin/v1/en/* $gh_pages_directory/spot/v1/en/
 }
 
